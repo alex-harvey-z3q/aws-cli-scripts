4251da747b87fd8515e26c8eb7b5b3a4dfd579eb
Alex Harvey <alexharv074@gmail.com>
2022-07-03 01:20:15 +1000

    Add features to manage_parameters.sh

--- manage_parameters.sh
+++ manage_parameters.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+export AWS_DEFAULT_OUTPUT="text"
 
 usage() {
   echo "Usage: bash $0 [-h] [-l]
-Usage: bash $0 -c SECRET_NAME -s SECRET
+Usage: bash $0 -l
+Usage: bash $0 -c SECRET_NAME -s SECRET [-o]
+Usage: bash $0 -c SECRET_NAME -s file://MYSECRET_FILE [-o]
 Usage: bash $0 -g SECRET_NAME
-Creates (-c), or gets (-g) a secret."
+Usage: bash $0 -d SECRET_NAME
+Lists (-l), creates (-c), gets (-g), or deletes (-d) a secret."
   exit 1
 }
 
@@ -13,12 +18,15 @@ get_opts() {
 
   cmd=(aws ssm)
 
-  while getopts "hs:c:g:" opt ; do
+  while getopts "hlc:og:s:d:" opt ; do
     case "$opt" in
       h) usage ;;
+      l) cmd+=(describe-parameters --query "Parameters[].[Name,Description]") ;;
       c) cmd+=(put-parameter --name "$OPTARG") ;;
-      g) cmd+=(get-parameters --name "$OPTARG" --with-decryption) ;;
+      o) cmd+=(--overwrite) ;;
+      g) cmd+=(get-parameters --name "$OPTARG" --with-decryption --query "Parameters[].Value") ;;
       s) cmd+=(--value "$OPTARG" --type "String") ;;
+      d) cmd+=(delete-parameters --name "$OPTARG") ;;
       # TODO. Add more features like delete etc here.
       \?) usage ;;
     esac

c3b8ed226ef588d6e4693aa6a201e157801eb583
Alex Harvey <alexharv074@gmail.com>
2022-07-03 01:19:42 +1000

    Add installer

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+this_script="$(basename "$0")"
+current_dir="$(basename "$(pwd)")"
+
+if [[ "$current_dir" != "aws-cli-scripts" ]] ; then
+  echo "Run this script from the aws-cli-scripts repo"
+fi
+
+_cp() {
+  echo "cp $*"
+  cp $*
+}
+
+for file_name in *.sh *.py ; do
+  [[ "$file_name" == "$this_script" ]] && continue
+  _cp "$file_name" /usr/local/bin
+done

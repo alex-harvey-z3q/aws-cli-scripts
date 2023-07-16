79da9a6776d05c7a2ffed93d0871d57d40edfd2e
Alex Harvey <alexharv074@gmail.com>
2023-07-16 23:03:10 +1000

    Improvement to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+this_dir="$(pwd)"
+
 this_script="$(basename "$0")"
 current_dir="$(basename "$(pwd)")"
 
@@ -8,10 +10,21 @@ if [[ "$current_dir" != "aws-cli-scripts" ]] ; then
 fi
 
 _cp() {
-  (set -x ; cp $*)
+  (set -x ; cp -p "$1" /usr/local/bin/"$2")
 }
+
+_link() {
+  (set -x ; cd /usr/local/bin && ln -sf $*)
+}
+
+if [[ "$1" == "-l" ]]; then
+  function=_link
+else
+  function=_cp
+fi
 
 for file_name in *.sh *.awk ; do
   [[ "$file_name" == "$this_script" ]] && continue
-  _cp "$file_name" /usr/local/bin
+  new_name="$(basename "$file_name")"
+  "$function" "$this_dir"/"$file_name" "$new_name"
 done
